e67d25066e90b86cd21d02c7f8b9928062a1332e
Émile Nadeau <nadeau.emile@gmail.com>
2023-03-15 21:38:00 +0000

    do not yield same work packet multiple time

--- src/searcher/queue.rs
+++ src/searcher/queue.rs
@@ -1,5 +1,5 @@
 use crate::pack::{StrategyFactory, StrategyPack};
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 
 pub struct ClassQueue<F: StrategyFactory> {
     pack: StrategyPack<F>,
@@ -7,6 +7,7 @@ pub struct ClassQueue<F: StrategyFactory> {
     curr_label: usize,
     strat_index: usize,
     max_strat_index: usize,
+    ignore: HashSet<usize>,
 }
 
 impl<F: StrategyFactory> ClassQueue<F> {
@@ -18,6 +19,7 @@ impl<F: StrategyFactory> ClassQueue<F> {
             curr_label: start_label,
             strat_index: 0,
             max_strat_index: pack_size,
+            ignore: HashSet::new(),
         }
     }
 
@@ -25,16 +27,28 @@ impl<F: StrategyFactory> ClassQueue<F> {
         self.queue.push_back(label);
     }
 
+    pub fn ignore(&mut self, label: usize) {
+        self.ignore.insert(label);
+    }
+
     pub fn next(&mut self) -> Option<(usize, &F)> {
+        loop {
+            let next = self.next_no_ignore()?;
+            if !self.ignore.contains(&next.0) {
+                return Some((next.0, self.pack.get_strategy_factory(next.1)));
+            }
+        }
+    }
+
+    /// Return the next logical work packet
+    fn next_no_ignore(&mut self) -> Option<(usize, usize)> {
         if self.max_strat_index == self.strat_index {
+            self.ignore(self.curr_label);
             self.strat_index = 1;
             self.curr_label = self.queue.pop_front()?;
         } else {
             self.strat_index += 1;
         }
-        Some((
-            self.curr_label,
-            self.pack.get_strategy_factory(self.strat_index - 1),
-        ))
+        Some((self.curr_label, self.strat_index - 1))
     }
 }
